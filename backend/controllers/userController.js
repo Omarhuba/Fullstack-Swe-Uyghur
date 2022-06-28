a86d183ab3b718735c911b0d803e27588ed76d5b
Omarhuba <omaruba@gmail.com>
2022-06-28 10:40:39 +0200

    fixt userController , updateUser, deleteUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const { isValidObjectId } = require("mongoose");
 const { User } = require("../models/userModel");
 const { Token } = require("../models/verificationToken");
-const { SendError } = require("../utils/helper");
+const { sendError } = require("../utils/helper");
 
 const getUser = async (req, res) => {
   try {
@@ -19,5 +19,39 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { getUser };
+const updateUser = async(req, res) => {
+  try{
+    const data = Object.keys(req.body)
+    const {_id} = req.user
+    const user = await User.findById({_id}).select('-password')
+
+    const allUsers = await User.find({}).exec()
+    const emailExist = allUsers.some((user => user.email === req.body.email))
+    if(emailExist) return sendError(res, 'Email already exist!!!!')
+
+    data.forEach((key) =>{
+      if(key && key !== 'role'){
+        user[key] = req.body[key]
+      }
+    })
+    await user.save()
+  }catch(err){
+    res.status(400).json(err.message)
+  }
+}
+
+
+const deleteUser = async(req, res)=>{
+  try{
+    const {email} = req.body
+    await User.deleteOne({email})
+    res.json('User Deleted!!!')
+  }catch(err){
+    res.status(400).json(err.message)
+  }
+}
+
+
+
+module.exports = { getUser, updateUser, deleteUser };
 
