3b6fe73582ea54d9e1147d4e21096d7ffb4e9c86
Omarhuba <omaruba@gmail.com>
2022-07-07 12:11:15 +0200

    some changes in loginController,

--- backend/controllers/auth/loginController.js
+++ backend/controllers/auth/loginController.js
@@ -6,13 +6,15 @@ const login = async(req, res)=>{
         // console.log(req.body);
         const user = await User.findOne({email}).exec()
         // console.log(user);
-        if(!user) throw new Error('invalid creadential')
-
+        if(!user) {
+            // throw new Error('invalid creadential')
+            res.json('Invalid Credential!!!!')
+        }
         user.comparePassword(password, async(err, isMached)=>{
             if(err){
                 throw new Error('login problem' + err)
             }else if(!isMached){
-                res.status(400).json('wrong password')
+                res.status(400).json('Invalid Password!!!')
             }else{
                 const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET,{expiresIn:'1d'})
                 const userData = await User.findOne({ _id: user._id }).select(
