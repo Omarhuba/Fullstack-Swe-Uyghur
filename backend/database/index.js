8988c7c86f873566e4ecb5b41ad7202f9b08b9d2
Omarhuba <omaruba@gmail.com>
2022-11-06 22:43:18 +0100

    added new connection string and it works

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { DATABASE_URI } = process.env;
 
 const sweuyghurDB = async () => {
   try {
-    await mongoose.connect(`${DATABASE_URI}sweuyghur`);
+    await mongoose.connect(process.env.DATABASE_URI);
+    // await mongoose.connect(`${DATABASE_URI}sweuyghur`);
     console.log("CONNECTED TO THE DB....");
   } catch (error) {
     console.log("Failed to connect to MongoDB", error);
